e6fbe0378e8dc5e43a8e93369e81fea4d218f71c
fajaralfa <11283360-fajaralfa@users.noreply.gitlab.com>
2024-02-10 05:38:17 +0700

    find array of needles string inside haystack

--- functions/other.php
+++ functions/other.php
@@ -8,3 +8,21 @@ function redirect($target)
     header("location: {$target}"); // set location menjadi target
     die; // berfungsi untuk berhenti menjalankan apapun setelah mengalihkan halaman
 }
+
+/**
+ * mengecek apakah string $haystack berisikan string dari array $needle
+ */
+function str_has(string $haystack, array $needles)
+{
+    $flag = true;
+    foreach ($needles as $t) {
+        if (str_contains($haystack, $t)) {
+            continue;
+        } else {
+            $flag = false;
+            break;
+        }
+    }
+
+    return $flag;
+}
