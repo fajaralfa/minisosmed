18fe3c2807120d3cfd9477466303a3e52aa4a999
fajaralfa <11283360-fajaralfa@users.noreply.gitlab.com>
2024-02-10 05:27:55 +0700

    [move {js,css} -> assets/{js,css}]

--- layout/header.php
+++ layout/header.php
@@ -7,8 +7,8 @@
     <!-- Tampilkan $title, jika $title tidak ada, maka tampilkan 'Galeri' -->
     <title><?= $title ?? 'Galeri' ?></title>
     <!-- bootstrap -->
-    <link rel="stylesheet" href="/galeri/css/bootstrap.min.css">
-    <script src="/galeri/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/galeri/assets/css/bootstrap.min.css">
+    <script src="/galeri/assets/js/bootstrap.min.js"></script>
 </head>
 
 <body>
